623287e9826e975e8ae9a23e285cd7ab565ff861
SyntienceS <nilsskulte@gmail.com>
2024-06-06 18:29:17 +0200

    revamp

--- about_us.php
+++ about_us.php
@@ -2,7 +2,7 @@
 <html>
 <head>
 <link rel="icon" type="image/png" href="logo.png" />
-<link rel="stylesheet" href="style2.css">
+<link rel="stylesheet" href="laravel\resources\css\style2.css">
 </head>
 <body>
 <div class="container">
@@ -18,31 +18,16 @@
     <p>
       <main>
         <section class="introduction">
-            <h2>What are we?</h2>
-            <p>We aim to provide the best auction house experience that you will have the pleasure of using.</p>
+            <h2>This page is currently not finished</h2>
+            <p>...</p>
         </section>
         <section class="temp_name">
-            <h2>The Neon International Team</h2>
+            <h2>Lost Heavy Inquisition</h2>
             <p></p>
         </section>
         <section id="" style="display: none;">
             <h2></h2>
-
-            <div class="worker">
-                <img src= "LigaVilla.jpg">
-                <h3>Līga Villa</h3>
-            </div>
-
-            <div class="worker">
-                <img src="NilsSkulte.jpg">
-                <h3>Nils Skulte</h3>
-            </div>
-
-            <div class="worker">
-                <img src="ModrisSkulte.jpg">
-                <h3>ModrisSkulte.jpg</h3>
-            </div>
     </p>
   </ul>
-  </nav>
+</nav>
 </div>
