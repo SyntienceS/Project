9196df722153c9f436502d49877b8074987031e7
SyntienceS <nilsskulte@gmail.com>
2025-06-13 11:09:17 +0300

    tailwindcss

--- vite.config.js
+++ vite.config.js
@@ -16,9 +16,11 @@ export default defineConfig({
 import { defineConfig } from "vite";
 import laravel from "laravel-vite-plugin";
 import vue from "@vitejs/plugin-vue";
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
     plugins: [
+        tailwindcss(),
         vue(),
         laravel({
             input: ["resources/css/app.css", "resources/js/app.js"],
